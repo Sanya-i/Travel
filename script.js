4a6b030180a689f08b552f9025d6252468b6e151
Sanya-i <r-223@yandex.ru>
2022-09-10 10:19:15 +0300

    feat: add full burger

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const burger = document.querySelector(".header__nav");
 const burgerMenu = document.querySelector(".burger");
 const close = document.querySelector(".close");
-const bg = document.querySelector(".black__burger");
+const bg = document.querySelector(".black");
+const link = document.querySelectorAll(".header__link")
 
 
 burgerMenu.addEventListener("click", () => {
    burger.classList.add("header__nav__active")
+   bg.classList.add("black__burger")
 })
+
+close.addEventListener("click", () => {
+   burger.classList.remove("header__nav__active")
+   bg.classList.remove("black__burger")
+})
+bg.addEventListener("click", () => {
+   burger.classList.remove("header__nav__active")
+   bg.classList.remove("black__burger")
+})
+
+link.forEach(element => {
+   element.addEventListener("click", () => {
+      burger.classList.remove("header__nav__active")
+      bg.classList.remove("black__burger")
+   })
+   
+});
